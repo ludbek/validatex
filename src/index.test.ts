1ca78eccba65a4d88e535f4e6a6ab5d33ccd9730
suren <sth.srn@gmail.com>
2021-06-27 18:29:54 +1000

    v2 init

---
+++ src/index.test.ts
@@ -0,0 +1,385 @@
+import {
+  string,
+  optional,
+  number,
+  object,
+  array,
+  boolean,
+  partial,
+  toggle,
+  decoder,
+  identity,
+  ValidationError,
+} from './index';
+
+describe('decoder', () => {
+  it('works without any option', () => {
+    const defaultErrorMsg = 'Not a string';
+    const string = decoder<string, unknown>({
+      typeGuard: (val: unknown): val is string => typeof val === 'string',
+      defaultErrorMsg: () => defaultErrorMsg,
+      defaultParser: identity,
+    });
+
+    const nameSchema = string();
+    expect(nameSchema('foo')).toEqual('foo');
+    expect(nameSchema.bind(nameSchema, 1)).toThrow(defaultErrorMsg);
+  });
+
+  it('works with DecoderFullOption', () => {
+    const defaultErrorMsg = 'Not a string';
+    const customErrorMsg = 'It should be a string.';
+    const customValidationErrorMsg = "must not contain '-'.";
+    const string = decoder<string, unknown>({
+      typeGuard: (val: unknown): val is string => typeof val === 'string',
+      defaultErrorMsg: () => defaultErrorMsg,
+      defaultParser: identity,
+    });
+    const alphaNumSchema = string({
+      parse(val) {
+        if (typeof val === 'string' || typeof val === 'number') return `${val}`;
+        return val;
+      },
+      validate(val, { key } = { key: undefined }) {
+        if (val.includes('-'))
+          throw new ValidationError(`${key} ${customValidationErrorMsg}`);
+      },
+      errorMsg: customErrorMsg,
+    });
+    expect(alphaNumSchema('valid string')).toEqual('valid string');
+    // it parses
+    expect(alphaNumSchema(1)).toEqual('1');
+    // it respects custom error msg
+    expect(alphaNumSchema.bind(alphaNumSchema, true)).toThrow(customErrorMsg);
+    // it respects custom validator
+    expect(
+      alphaNumSchema.bind(alphaNumSchema, 'hello-there', { key: 'akey' })
+    ).toThrow(customValidationErrorMsg);
+
+    // it respects getErrorMsg
+    const schema = string({
+      getErrorMsg: (val, { key } = { key: undefined }) => {
+        return `${key} must not be ${val}`;
+      },
+    });
+    expect(schema.bind(schema, 1, { key: 'akey' })).toThrow(
+      'akey must not be 1'
+    );
+  });
+
+  it('works with DecoderValidator', () => {
+    const defaultErrorMsg = 'Not a string';
+    const string = decoder<string, unknown>({
+      typeGuard: (val: unknown): val is string => typeof val === 'string',
+      defaultErrorMsg: () => defaultErrorMsg,
+      defaultParser: identity,
+    });
+    const fooSchema = string(val => {
+      if (val !== 'foo') throw new ValidationError("Its not 'foo'.");
+    });
+    expect(fooSchema.bind(fooSchema, 'bar')).toThrow("Its not 'foo'.");
+  });
+
+  it('works with CustomErrorMsg', () => {
+    const defaultErrorMsg = 'Not a string';
+    const string = decoder<string, unknown>({
+      typeGuard: (val: unknown): val is string => typeof val === 'string',
+      defaultErrorMsg: () => defaultErrorMsg,
+      defaultParser: identity,
+    });
+    const customErrorMsg = 'A custom error msg.';
+    const fooSchema = string(customErrorMsg);
+    expect(fooSchema.bind(fooSchema, 1)).toThrow(customErrorMsg);
+  });
+});
+
+describe('string', () => {
+  it('throws an error is a value is not a string', () => {
+    function run() {
+      return string()(1);
+    }
+    expect(run).toThrow('Not a string');
+  });
+
+  it('returns the value if it is a string', () => {
+    const val = 'a string';
+    expect(string()(val)).toEqual(val);
+  });
+});
+
+describe('number', () => {
+  it('throws an error is a value is not a string', () => {
+    function run() {
+      return number()('a string');
+    }
+    expect(run).toThrow('Not a number');
+  });
+
+  it('returns the value if it is a string', () => {
+    const val = 1;
+    expect(number()(val)).toEqual(val);
+  });
+});
+
+describe('boolean', () => {
+  it('throws an error if a value is not a boolean', () => {
+    const boolSchema = boolean();
+    expect(boolSchema.bind(boolSchema, 1)).toThrow('Not a boolean');
+  });
+
+  it('returns value if it is a boolean', () => {
+    const boolSchema = boolean();
+    expect(boolSchema(false)).toEqual(false);
+    expect(boolSchema(true)).toEqual(true);
+  });
+});
+
+describe('object', () => {
+  it('returns a value if its valid', () => {
+    const userSchema = object({ name: string(), address: string() });
+    const val = { name: 'foo', address: 'bar' };
+    expect(userSchema(val)).toEqual(val);
+  });
+
+  it('throws the errors if a value is invalid', () => {
+    const userSchema = object({ name: string(), address: string() });
+    const val = {};
+    function run() {
+      return userSchema(val);
+    }
+    expect(run).toThrow('{"name":"Not a string","address":"Not a string"}');
+  });
+
+  it('validates non objects', () => {
+    const userSchema = object({ name: string(), address: string() });
+    expect(userSchema.bind(userSchema, undefined)).toThrow(
+      '{"name":"Not a string","address":"Not a string"}'
+    );
+    expect(userSchema.bind(userSchema, 1)).toThrow(
+      '{"name":"Not a string","address":"Not a string"}'
+    );
+  });
+
+  it('respects custom validator', () => {
+    const expectedError = { confirm: 'It must match "password".' };
+    const customValidator = (val: { password: string; confirm: string }) => {
+      const { password, confirm } = val;
+      if (password !== confirm) {
+        throw new ValidationError(expectedError);
+      }
+    };
+    const signupSchema = object(
+      {
+        username: string(),
+        password: string(),
+        confirm: string(),
+      },
+      customValidator
+    );
+    const rawData = {
+      username: 'ausername',
+      password: 'apassword',
+      confirm: 'bpassword',
+    };
+    expect(signupSchema.bind(signupSchema, rawData)).toThrow(
+      JSON.stringify(expectedError)
+    );
+  });
+
+  it('throws if unknown fields are passed', () => {
+    const loginSchema = object({
+      username: string(),
+      password: string(),
+    });
+    const data = {
+      username: 'a username',
+      password: 'a password',
+      unknownField1: 'unknown field 1',
+      unknownField2: 'unknown field 2',
+    };
+    const expectedError =
+      '{"unknownField1":"Unknown field.","unknownField2":"Unknown field."}';
+    expect(loginSchema.bind(loginSchema, data)).toThrow(expectedError);
+  });
+
+  it('respects "unknownFieldErrorMsg" option', () => {
+    const loginSchema = object(
+      {
+        username: string(),
+        password: string(),
+      },
+      { unknownFieldErrorMsg: 'This field is not allowed.' }
+    );
+    const data = {
+      username: 'a username',
+      password: 'a password',
+      unknownField1: 'unknown field 1',
+      unknownField2: 'unknown field 2',
+    };
+    const expectedError =
+      '{"unknownField1":"This field is not allowed.","unknownField2":"This field is not allowed."}';
+    expect(loginSchema.bind(loginSchema, data)).toThrow(expectedError);
+  });
+
+  it('does not throw if strict is false', () => {
+    const loginSchema = object(
+      {
+        username: string(),
+        password: string(),
+      },
+      { strict: false }
+    );
+    const data = {
+      username: 'a username',
+      password: 'a password',
+      unknownField1: 'unknown field 1',
+      unknownField2: 'unknown field 2',
+    };
+    expect(loginSchema(data)).toEqual(
+      expect.objectContaining({
+        username: data.username,
+        password: data.password,
+      })
+    );
+  });
+});
+
+describe('array', () => {
+  it('returns value if valid', () => {
+    const namesSchema = array(string());
+    const value = ['foo', 'bar'];
+    expect(namesSchema(value)).toEqual(value);
+  });
+
+  it('throws error if a value is invalid', () => {
+    const namesSchema = array(string());
+    const value = [1, undefined];
+    const expectedError = [
+      { _index: 0, error: 'Not a string' },
+      { _index: 1, error: 'Not a string' },
+    ];
+    expect(namesSchema.bind(namesSchema, value)).toThrow(
+      JSON.stringify(expectedError)
+    );
+  });
+
+  it('validates non array value', () => {
+    const namesSchema = array(string());
+    const expectedError = [{ _index: 0, error: 'Not a string' }];
+    expect(namesSchema.bind(namesSchema, undefined)).toThrow(
+      JSON.stringify(expectedError)
+    );
+  });
+
+  it('validates empty array', () => {
+    const namesSchema = array(string());
+    const expectedError = [{ _index: 0, error: 'Not a string' }];
+    expect(namesSchema.bind(namesSchema, [])).toThrow(
+      JSON.stringify(expectedError)
+    );
+  });
+
+  it('validates empty array', () => {
+    const expectedError = 'Only 2 items are allowed';
+    function allowOnly2(values: string[]) {
+      if (values.length > 2) throw new ValidationError(expectedError);
+    }
+    const namesSchema = array(string(), allowOnly2);
+    expect(namesSchema.bind(namesSchema, ['a', 'b', 'c', 'd'])).toThrow(
+      JSON.stringify(expectedError)
+    );
+  });
+});
+
+describe('optional', () => {
+  it('returns undefined if the given value is undefined', () => {
+    expect(optional(string())(undefined)).toEqual(undefined);
+  });
+
+  it('returns a value if the given value is not a undefined', () => {
+    const val = 'a string';
+    expect(optional(string())(val)).toEqual(val);
+  });
+});
+
+describe('partial', () => {
+  it('makes all the fields optional', () => {
+    const userSchema = partial({
+      username: string(),
+      password: string(),
+    });
+    expect(userSchema({})).toEqual({});
+  });
+
+  it('works with toggle', () => {
+    const userSchema = partial({
+      username: string(),
+      password: toggle(string()),
+    });
+    expect(userSchema.bind(userSchema, {})).toThrow('');
+  });
+
+  it('makes all the fields optional', () => {
+    const expectedError = {
+      displayError: '"username" and "password" cannot be same.',
+    };
+    function customValidator({
+      username,
+      password,
+    }: {
+      username: string;
+      password: string;
+    }) {
+      if (username === password) throw new ValidationError(expectedError);
+    }
+    const userSchema = partial(
+      {
+        username: string(),
+        password: string(),
+      },
+      customValidator
+    );
+    const data = { username: 'a', password: 'a' };
+    expect(userSchema.bind(userSchema, data)).toThrow(
+      JSON.stringify(expectedError)
+    );
+  });
+});
+
+describe('kitchen sink', () => {
+  it('works', () => {
+    const userSchema = array(
+      object({
+        name: string(),
+        mobile: toggle(number()),
+        address: partial({
+          postcode: number(),
+          street1: string(),
+          street2: toggle(string()),
+        }),
+      })
+    );
+
+    const value1 = [
+      {
+        name: 'foo',
+        mobile: 1234567890,
+        address: { postcode: 1, street1: 'street 1', street2: 'street 2' },
+      },
+    ];
+    expect(userSchema(value1)).toEqual(value1);
+    const value2 = [
+      {
+        name: 'foo',
+        mobile: 1234567890,
+        address: { postcode: 1, street1: 'street 1', street2: 'street 2' },
+      },
+      {
+        address: { street1: 'street 1' },
+      },
+    ];
+    const expectedError2 =
+      '[{"_index":1,"error":{"name":"Not a string","address":{"street2":"Not a string"}}}]';
+    expect(userSchema.bind(userSchema, value2)).toThrow(expectedError2);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,281 @@
+export type ErrorMsg = string | Record<string, any>;
+export type Context = {
+  key?: string;
+  index?: number;
+};
+export type Decoder = (value: any, context?: Context) => any;
+export interface Switch<T> {
+  decoder: T;
+}
+export type Schema = Record<string, Decoder | Switch<any>>;
+export type TypeOf<T extends Decoder> = ReturnType<T>;
+export type FilterOptionalKeys<T> = {
+  [K in keyof T]: T[K] extends Switch<any> ? K : never;
+}[keyof T];
+export type FilterRequiredKeys<T> = {
+  [K in keyof T]: T[K] extends Switch<any> ? never : K;
+}[keyof T];
+export type MergeIntersection<T extends Schema> = {
+  [K in FilterOptionalKeys<T>]?: T[K] extends Switch<any>
+    ? ReturnType<T[K]['decoder']>
+    : never;
+} &
+  { [K in FilterRequiredKeys<T>]: ReturnType<T[K]> };
+export type PartialMergeIntersection<T extends Schema> = {
+  [K in FilterOptionalKeys<T>]: T[K] extends Switch<any>
+    ? ReturnType<T[K]['decoder']>
+    : never;
+} &
+  { [K in FilterRequiredKeys<T>]?: ReturnType<T[K]> };
+export interface ObjectType {
+  [index: string]: any;
+}
+
+export type DecoderValidator<T> = (val: T, context?: Context) => void;
+export interface DecoderFullOption<T> {
+  parse?(val: any): T;
+  validate?: DecoderValidator<T>;
+  errorMsg?: string;
+  getErrorMsg?: (val: any, context?: Context) => string;
+}
+export type CustomErrorMsg = string;
+export type DecoderOption<T> =
+  | DecoderValidator<T>
+  | CustomErrorMsg
+  | DecoderFullOption<T>;
+
+export function isObject(val: any): val is ObjectType {
+  return typeof val === 'object' && val !== null;
+}
+
+export function isUndefined(val: any): val is undefined {
+  return typeof val === 'undefined';
+}
+
+export class ValidationError extends Error {
+  readonly error: ErrorMsg;
+  constructor(msg: ErrorMsg) {
+    super(JSON.stringify(msg));
+    this.error = msg;
+  }
+}
+
+export function toggle<T extends Decoder>(aDecoder: T): Switch<T> {
+  return {
+    decoder: aDecoder,
+  };
+}
+
+export function identity<T>(val: T): T {
+  return val;
+}
+
+function isDecoderValidator<T>(
+  decoderOption: any,
+): decoderOption is DecoderValidator<T> {
+  return typeof decoderOption === 'function';
+}
+
+function isCustomErrorMsg(decoderOption: any): decoderOption is CustomErrorMsg {
+  return typeof decoderOption === 'string';
+}
+
+interface DecoderParams<T, U> {
+  typeGuard: (rawVal: unknown) => rawVal is T;
+  defaultErrorMsg: (val: any, context?: Context) => string;
+  defaultParser: (rawVal: U) => T | U;
+}
+export function decoder<T, U>({
+  typeGuard,
+  defaultErrorMsg,
+  defaultParser,
+}: DecoderParams<T, U>) {
+  return (options: DecoderOption<T> = {}) => {
+    return (val: any, context?: Context): T => {
+      val = defaultParser(val);
+      options = isDecoderValidator<T>(options)
+        ? { validate: options }
+        : options;
+      options = isCustomErrorMsg(options) ? { errorMsg: options } : options;
+      const { parse, validate, errorMsg, getErrorMsg } = options;
+      val = parse ? parse(val) : val;
+      if (!typeGuard(val))
+        throw new ValidationError(
+          errorMsg ||
+            (getErrorMsg && getErrorMsg(val, context)) ||
+            defaultErrorMsg(val, context),
+        );
+      if (validate) {
+        validate(val, context);
+      }
+      return val;
+    };
+  };
+}
+
+export const string = decoder<string, unknown>({
+  typeGuard: (val: unknown): val is string => typeof val === 'string',
+  defaultErrorMsg: () => 'Not a string',
+  defaultParser: identity,
+});
+
+export const number = decoder<number, unknown>({
+  typeGuard: (val: unknown): val is number => typeof val === 'number',
+  defaultErrorMsg: () => 'Not a number',
+  defaultParser: identity,
+});
+
+export const boolean = decoder<boolean, unknown>({
+  typeGuard: (val: unknown): val is boolean => typeof val === 'boolean',
+  defaultErrorMsg: () => 'Not a boolean',
+  defaultParser: identity,
+});
+
+function isDecoder(v: unknown): v is Decoder {
+  return typeof v === 'function';
+}
+
+export function optional(aDecoder: Decoder) {
+  return <T>(val: any, context?: Context): T | undefined => {
+    if (val === undefined) return undefined;
+    return aDecoder(val, context);
+  };
+}
+
+type ValueError = {
+  key: string;
+  value: any;
+  error?: string;
+};
+
+function mapValueError(data: any) {
+  return ([key, aDecoder]: [string, Decoder | Switch<any>]): ValueError => {
+    try {
+      const actualDecoder = isDecoder(aDecoder)
+        ? aDecoder
+        : optional(aDecoder.decoder);
+      return {
+        key,
+        value: actualDecoder(data[key], { key }),
+        error: undefined,
+      };
+    } catch (e) {
+      if (e.error !== undefined) {
+        return { key, value: undefined, error: e.error };
+      }
+      throw e;
+    }
+  };
+}
+export function getUnknownFields(inputKeys: string[], schemaKeys: string[]) {
+  return inputKeys.filter(key => !schemaKeys.includes(key));
+}
+
+export function constructUnknownFieldsError(
+  unknownKeys: string[],
+  unknownFieldErrorMsg: string,
+) {
+  return unknownKeys.reduce(
+    (a, k) => ({ ...a, [k]: unknownFieldErrorMsg }),
+    {},
+  );
+}
+
+interface ObjectFullOption<T> {
+  strict?: boolean;
+  unknownFieldErrorMsg?: string;
+  validate?: DecoderValidator<T>;
+}
+type ObjectOption<T> = DecoderValidator<T> | ObjectFullOption<T>;
+
+const defaultOption = { strict: true, unknownFieldErrorMsg: 'Unknown field.' };
+export function object<T extends Schema, U extends MergeIntersection<T>>(
+  schema: T,
+  option?: ObjectOption<{ [K in keyof U]: U[K] }>,
+) {
+  return (rawData: any, context?: Context): { [K in keyof U]: U[K] } => {
+    rawData = isObject(rawData) ? rawData : {};
+
+    // convert option to ObjectFullOption
+    option = isDecoderValidator(option) ? { validate: option } : option;
+    option = isUndefined(option)
+      ? defaultOption
+      : { ...defaultOption, ...option };
+    const { validate, strict, unknownFieldErrorMsg } = option as any;
+
+    // handle unknown fields
+    if (strict) {
+      const unknownKeys = getUnknownFields(
+        Object.keys(rawData),
+        Object.keys(schema),
+      );
+      if (unknownKeys.length !== 0) {
+        throw new ValidationError(
+          constructUnknownFieldsError(unknownKeys, unknownFieldErrorMsg),
+        );
+      }
+    }
+
+    // handle known fields
+    const { data, error } = Object.entries(schema)
+      .map(mapValueError(rawData))
+      .reduce(
+        (a, valueError) => {
+          const { key, value, error: anError } = valueError;
+          if (anError !== undefined) {
+            return { ...a, error: { ...a.error, [key]: anError } };
+          }
+          return { ...a, data: { ...a.data, [key]: value } };
+        },
+        { data: {}, error: {} },
+      );
+
+    if (Object.keys(error).length !== 0) throw new ValidationError(error);
+
+    if (validate) validate(data as any, context);
+    return data as any;
+  };
+}
+
+export function partial<
+  T extends Schema,
+  U extends PartialMergeIntersection<T>
+>(schema: T, customValidator?: DecoderValidator<{ [K in keyof U]: U[K] }>) {
+  schema = Object.keys(schema).reduce((a, k) => {
+    let aDecoder = schema[k];
+    aDecoder = isDecoder(aDecoder) ? optional(aDecoder) : aDecoder.decoder;
+    return { ...a, [k]: aDecoder };
+  }, {}) as any;
+  return (val: any, context?: Context): { [K in keyof U]: U[K] } => {
+    return object(schema, customValidator)(val, context) as any;
+  };
+}
+
+export function array<T extends Decoder>(
+  schema: T,
+  customValidator?: DecoderValidator<ReturnType<T>[]>,
+) {
+  return (data: any, context?: Context): ReturnType<T>[] => {
+    data = Array.isArray(data) ? data : [undefined];
+    data = data.length === 0 ? [undefined] : data;
+    const processedData = [];
+    const errors: Record<string, any>[] = [];
+    data.forEach((datum: any, index: number) => {
+      try {
+        processedData.push(schema(datum, { index }));
+      } catch (e) {
+        if (e.error !== undefined) {
+          errors.push({ _index: index, error: e.error });
+        } else {
+          throw e;
+        }
+      }
+    });
+
+    if (errors.length !== 0) {
+      throw new ValidationError(errors);
+    }
+    if (customValidator) customValidator(data, context);
+    return data;
+  };
+}

6e9a23d8f9bae0b9b106eacec0a342d19f16ddd7
xryuki_i <summitshrestha7@gmail.com>
2022-08-22 10:41:21 +0545

    Added regex, max, min and dateFormat validators and successfully tested them

--- src/validators.test.ts
+++ src/validators.test.ts
@@ -1,6 +1,7 @@
 import { Context } from './types';
-import { email, length } from './validators';
+import { dateFormat, email, length, max, maxlength, min, minlength, pattern } from './validators';
 
+// Email Validator
 describe('email', () => {
   it('returns error if an email is invalid', () => {
     expect(email()('aninvalid.email')).toEqual('The email is not valid');
@@ -34,6 +35,7 @@ describe('email', () => {
   });
 });
 
+// Length validator
 describe('length', () => {
   it('returns error', () => {
     const expectedError = `Should be ${5} characters long`;
@@ -66,3 +68,102 @@ describe('length', () => {
     expect(got).toEqual(expectedError);
   });
 });
+
+// Pattern validator
+describe('pattern', () => {
+  it('returns error', () => {
+    const expectedError = `Invalid characters in input`;
+    expect(pattern(/^[a-b]/)('z')).toEqual(expectedError);
+  });
+  it('returns undefined', () => {
+    expect(pattern(/^[a-z0-9]*$/)('12ba')).toEqual(undefined);
+  });
+  it('considers errorMsg', () => {
+    const errorMsg = 'Custom error.';
+    expect(pattern(/^[a-z0-9]*$/, errorMsg)('abc_123')).toEqual(errorMsg);
+  });
+})
+
+// minlength validator
+describe('minlength', () => {
+  it('returns error', () => {
+    const expectedError = `Should be at least ${5} characters long`;
+    expect(minlength(5)("suku")).toEqual(expectedError);
+  });
+  it('returns undefined', () => {
+    expect(minlength(5)("summit")).toEqual(undefined);
+  });
+  it('considers errorMsg', () => {
+    const errorMsg = 'Custom error.';
+    expect(minlength(6, errorMsg)("suku")).toEqual(errorMsg);
+  });
+})
+
+// maxlength validator
+describe('maxlength', () => {
+  it('returns error', () => {
+    const length = 3
+    const expectedError = `Should be no longer than ${length} characters`;
+    expect(maxlength(length)("suku")).toEqual(expectedError);
+  });
+  it('returns undefined', () => {
+    expect(maxlength(6)("summit")).toEqual(undefined);
+  });
+  it('considers errorMsg', () => {
+    const errorMsg = 'Custom error.';
+    expect(maxlength(3, errorMsg)("suku")).toEqual(errorMsg);
+  });
+})
+
+// min validator
+describe('min', () => {
+  it('returns error', () => {
+    const digit = 5;
+    const expectedError = `Should be at least ${digit} digits long`;
+    expect(min(digit)(4037)).toEqual(expectedError);
+  });
+  it('returns undefined', () => {
+    expect(min(5)(348453)).toEqual(undefined);
+  });
+  it('considers errorMsg', () => {
+    const errorMsg = 'Custom error.';
+    expect(min(6, errorMsg)(4037)).toEqual(errorMsg);
+  });
+})
+
+// max validator
+describe('max', () => {
+  it('returns error', () => {
+    const digit = 3;
+    const expectedError = `Should be no longer than ${digit} digits`;
+    expect(max(digit)(4037)).toEqual(expectedError);
+  });
+  it('returns undefined', () => {
+    expect(max(6)(348453)).toEqual(undefined);
+  });
+  it('considers errorMsg', () => {
+    const digit = 3;
+    const errorMsg = `There must not be more than ${digit} digits)`;
+    expect(max(digit, errorMsg)(4037)).toEqual(errorMsg);
+  });
+})
+
+// date format validator
+describe('dateFormat', () => {
+  it('returns error', () => {
+    const expectedError = `Doesn't match the date format of MM/DD/YYYY`;
+    expect(dateFormat('MM/DD/YYYY')('13/04/2021')).toEqual(expectedError);
+  });
+  it('returns invalid format error', () => {
+    const expectedError = `Invalid date format specified`;
+    expect(dateFormat('MM/DD/YYY')('12/04/2021')).toEqual(expectedError);
+  });
+  it('returns undefined', () => {
+    expect(dateFormat('MM/DD/YYYY')('12/05/2021')).toEqual(undefined);
+  });
+  it('considers errorMsg', () => {
+    const option = 'YYYY/MM/DD';
+    const errorMsg = `The date entered doesn't match the format ${option}`;
+    expect(dateFormat(option, errorMsg)('04/13/2021')).toEqual(errorMsg);
+  });
+})
--- src/validators.ts
+++ src/validators.ts
@@ -55,7 +55,7 @@ export function maxlength(
   errorMsg?: CustomErrMsg<string, number>,
 ) {
   return (val: string, context?: Context) => {
-    const defaultErrorMsg = `Should be at least ${option} characters long`;
+    const defaultErrorMsg = `Should be no longer than ${option} characters`;
     if (option < val.length) {
       return getError({
         errorMsg,
@@ -98,9 +98,116 @@ export function length(
 }
 
 // regex
-// minNum
-// maxNum
+export function pattern(
+  option: RegExp,
+  errorMsg?: CustomErrMsg<string, RegExp>
+){
+  const defaultErrorMsg = 'Invalid characters in input';
+  return(val: string, context?: Context): string | undefined => {
+    return option.test(val)
+      ?undefined
+      :getError({
+        errorMsg,
+        defaultErrorMsg,
+        val,
+        context,
+        option,
+      });
+  }
+}
+
+// validator for minimum no of digits in a number
+export function min(
+  option: number,
+  errorMsg?: CustomErrMsg<number, number>,
+) {
+  return (val: number, context?: Context): string | undefined => {
+    const defaultErrorMsg = `Should be at least ${option} digits long`;
+    const num = val.toString()
+    return num.length < option
+      ? getError({ errorMsg, defaultErrorMsg, val, context, option })
+      : undefined 
+  }
+}
+
+// validator for maximum no of digits in a number
+export function max(
+  option: number,
+  errorMsg?: CustomErrMsg<number, number>,
+) {
+  return (val: number, context?: Context): string | undefined => {
+    const defaultErrorMsg = `Should be no longer than ${option} digits`;
+    const num = val.toString()
+    return num.length > option
+      ? getError({ errorMsg, defaultErrorMsg, val, context, option })
+      : undefined 
+  }
+}
+
 // dateFormat
+export function dateFormat(
+  option: string,
+  errorMsg?: CustomErrMsg<string, string>,
+) {
+  return (val: string, context?: Context): string | undefined => {
+    const defaultErrorMsg = `Doesn't match the date format of ${option}`
+    let dateRegex:RegExp = /^/;
+    let posYear = 0;
+    let posMonth = 0;
+    let posDay = 0;
+
+    switch(option){
+      case "MM/DD/YYYY":
+        dateRegex = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+        posDay = 1;
+        posYear = 2;
+        break;
+        
+      case "DD/MM/YYYY":
+        dateRegex = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+        posMonth = 1;
+        posYear = 2;
+        break;
+        
+      case "YYYY/MM/DD":
+        dateRegex = /^\d{4}\/\d{1,2}\/\d{1,2}$/;
+        posMonth = 1;
+        posDay = 2;
+        break;
+
+      default:
+        let defaultErrorMsg = "Invalid date format specified"
+        return getError({ errorMsg, defaultErrorMsg, val, context, option })
+    }
+
+    if (!dateRegex.test(val)) 
+      return getError({ errorMsg, defaultErrorMsg, val, context, option })
+      
+    //Parsing the date part to integers
+    var parts = val.split("/");
+    var day = parseInt(parts[posDay], 10);
+    var month = parseInt(parts[posMonth], 10);
+    var year = parseInt(parts[posYear], 10);
+
+    //Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+      return getError({ errorMsg, defaultErrorMsg, val, context, option })
+
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    if (day > 0 && day <= monthLength[month - 1]){
+      return undefined;
+    }else{
+      return getError({ errorMsg, defaultErrorMsg, val, context, option })
+    }
+  }
+}
+
 // minDate
 // maxDate
 
